d4cac069f8f4a5d1f2efb979a01e8d27f8812e09
taeyzzz <tkul@sertiscorp.com>
2019-12-11 18:19:36 +0700

    handle preview DONE

--- src/App.js
+++ src/App.js
@@ -4,32 +4,7 @@ import './App.css';
 import './style.scss'
 
 class App extends React.Component{
-  componentDidMount() {
-    let div = this.dropZone
-    div.addEventListener('dragenter', this.handleDragIn)
-    div.addEventListener('dragleave', this.handleDragOut)
-    div.addEventListener('dragover', this.handleDrag)
-    div.addEventListener('drop', this.handleDrop)
-  }
-  componentWillUnmount() {
-    let div = this.dropZone
-    div.removeEventListener('dragenter', this.handleDragIn)
-    div.removeEventListener('dragleave', this.handleDragOut)
-    div.removeEventListener('dragover', this.handleDrag)
-    div.removeEventListener('drop', this.handleDrop)
-  }
-
   handleDrag = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    console.log(e);
-  }
-  handleDragIn = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    console.log(e);
-  }
-  handleDragOut = (e) => {
     e.preventDefault()
     e.stopPropagation()
     console.log(e);
@@ -40,14 +15,20 @@ class App extends React.Component{
     e.stopPropagation()
     console.log(e);
     console.log(e.dataTransfer.files);
+    const previewImage = document.getElementById("preview")
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      previewImage.src = e.target.result
+    }
+    reader.readAsDataURL(e.dataTransfer.files[0]);
   }
 
   render(){
     return (
       <div className="page">
         Page
-        <div ref={ref => this.dropZone = ref} className="drop-zone">
-
+        <div ref={ref => this.dropZone = ref} className="drop-zone" onDrag={this.handleDrag} onDragOver={this.handleDrag} onDrop={this.handleDrop}>
+          <img id="preview" />
         </div>
       </div>
     )
